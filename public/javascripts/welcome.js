1b4a6bccd890c93a2f5be8e069ae89f20ab6552c
AssKickerSurfaceBook <121584565@163.com>
2018-09-18 21:34:53 +0800

    更改主页，嵌入MDEditor

--- public/javascripts/welcome.js
+++ public/javascripts/welcome.js
@@ -1,12 +1,12 @@
 /**
  * Created by ian0214 on 2017/8/28.
  */
-var caption = new Vue({
-    el: '#caption',
-    data: {
-        content: 'Welcome'
-    }
-});
+// var caption = new Vue({
+//     el: '#caption',
+//     data: {
+//         content: 'Welcome'
+//     }
+// });
 
 // nav is imported from navigation.js
 nav.Index = true;
